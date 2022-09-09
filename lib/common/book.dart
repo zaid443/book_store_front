31615eb8d0394ed7a63ed426ee978426e13fd377
zaid443 <ziadhaider9@gmail.com>
2022-09-10 01:38:11 +0300

    horizontal slider

---
+++ lib/common/book.dart
@@ -0,0 +1,9 @@
+class Books {
+  final String cover;
+  final String name;
+  final String auther;
+  final String discrption;
+  final int rate;
+
+  Books({required this.cover,required this.name, required this.auther, required this.discrption,required this.rate});
+}
--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -1,12 +1,15 @@
-// ignore_for_file: unnecessary_string_escapes
+// ignore_for_file: unnecessary_string_escapes, non_constant_identifier_names
 
+import 'package:book_store/common/default_book.dart';
 import 'package:book_store/view/home/shape.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:scroll_snap_list/scroll_snap_list.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+import '../../common/book.dart';
 
 class Home extends StatelessWidget {
   Home({Key? key}) : super(key: key);
@@ -40,8 +43,56 @@ class Home extends StatelessWidget {
                 color: Colors.white,
                 height: 35,
               )),
+          Container(
+            height: 200,
+            margin: const EdgeInsets.only(top: 150),
+            child: CarouselSlider(
+              items: imgPath
+                  .map((item) => Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 5),
+                        child: Center(
+                          child: Image.asset(
+                            item,
+                            fit: BoxFit.cover,
+                            width: 500,
+                          ),
+                        ),
+                      ))
+                  .toList(),
+              options: CarouselOptions(
+                  aspectRatio: 2.0,
+                  enlargeCenterPage: true,
+                  enableInfiniteScroll: true,
+                  initialPage: 3,
+                  autoPlay: true,
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  height: 400),
+            ),
+          )
         ],
       ),
     );
   }
 }
+
+// Widget AnimList(BuildContext context, int index) {
+//   Books book = default_book_list[index];
+//   return Container(
+
+//     margin: EdgeInsets.symmetric(horizontal: 50),
+//     child: Expanded(
+//       child: Image.asset(book.cover),
+//     ),
+//   );
+// }
+
+// Widget CircularSlider(String imgPath, int index) {
+//   Books book = default_book_list[index];
+//   return Container(
+//     height: 200,
+//     child: Image.asset(
+//       book.cover,
+//       fit: BoxFit.cover,
+//     ),
+//   );
+// }
